a159fffe3961774bd8f615f3c7843fbc291b84ad
ppa <p.pares@castsoftware.com>
2019-12-23 11:32:24 +0100

    Look on ED domains (1)

--- datamart.bat
+++ datamart.bat
@@ -1,31 +1,28 @@
 @echo off
 
-SET HD_ROOT=http://localhost:9090/CAST-RESTAPI/rest
-SET ED_ROOT=http://localhost:9090/CAST-RESTAPI/rest
+SET HD_ROOT=http://localhost:9090/Health/rest
+SET ED_ROOT=http://localhost:9090/Engineering/rest
+SET ED_DOMAINS=ED1,ED2
 
 if [%1] == [install] goto :INSTALL
 if [%1] == [refresh] goto :REFRESH
 
-echo Extract/Transform/Load data from AAD and ED domains
 echo Usage is
 echo datamart install
-echo    To create or re-recreate the tables of datamart schema for a first install or an upgrade
+echo    To create tables and extract data from AAD and all ED domains
 echo datamart refresh
-echo    To truncate the tables of datamart schema for a common usage
+echo    To truncate tables and extract data from AAD and all ED domains
 goto :FAIL
 
 :INSTALL
-call run install %HD_ROOT% AAD          || goto :FAIL
-call run merge   %ED_ROOT% ECHO         || goto :FAIL
-call run merge   %ED_ROOT% HL           || goto :FAIL
+call run install %HD_ROOT% AAD || goto :FAIL
+FOR %%D IN (%ED_DOMAINS%) DO (call run merge %ED_ROOT% %%D || goto :FAIL)
 GOTO :EOF
 
 :REFRESH
-call run refresh %HD_ROOT% AAD          || goto :FAIL
-call run merge   %ED_ROOT% ECHO         || goto :FAIL
-call run merge   %ED_ROOT% HL           || goto :FAIL
+call run refresh %HD_ROOT% AAD || goto :FAIL
+FOR %%D IN (%ED_DOMAINS%) DO (call run merge %ED_ROOT% %%D || goto :FAIL)
 GOTO :EOF
-
 
 :FAIL
 EXIT /b 1
