ff201f463f196edb1cd67ab0ceda69f806105a63
ppa <p.pares@castsoftware.com>
2020-01-15 10:41:16 +0100

    Add OMG/CISQ view and OWASP-2017 views

---
+++ views/DIM_OWASP_2017.sql
@@ -0,0 +1,16 @@
+DROP VIEW IF EXISTS :schema.dim_owasp_2017;
+CREATE OR REPLACE VIEW :schema.dim_owasp_2017 AS 
+  SELECT c.metric_id, c.rule_name, 
+    BOOL_OR(c.tag = 'A1-2017') AS a1_2017,
+    BOOL_OR(c.tag = 'A2-2017') AS a2_2017,
+    BOOL_OR(c.tag = 'A3-2017') AS a3_2017,
+    BOOL_OR(c.tag = 'A4-2017') AS a4_2017,
+    BOOL_OR(c.tag = 'A5-2017') AS a5_2017,
+    BOOL_OR(c.tag = 'A6-2017') AS a6_2017,
+    BOOL_OR(c.tag = 'A7-2017') AS a7_2017,
+    BOOL_OR(c.tag = 'A8-2017') AS a8_2017,
+    BOOL_OR(c.tag = 'A9-2017') AS a9_2017,
+    BOOL_OR(c.tag = 'A10-2017') AS a10_2017
+  FROM :schema.std_rules c 
+  JOIN :schema.std_rules r on r.metric_id = c.metric_id and r.tag = 'OWASP-2017'
+  GROUP BY 1,2;   
