c6edf18f15df17c7fd27ffb6bb6e113eac886bee
ppa <p.pares@castsoftware.com>
2020-02-18 16:11:06 +0100

    Swap command line arguments

--- utilities/fetch.py
+++ utilities/fetch.py
@@ -1,4 +1,6 @@
 import sys
+import signal
+import datetime
 import argparse
 import requests
 import threading
@@ -14,7 +16,7 @@ elapsed_time = 0
 is_timer_stopped = False
 
 def print_progress():
-    print("Time: " + str(elapsed_time) + "s - received: " + str(item) + "k \r", sep=' ', end='', flush=True, file=sys.stderr)
+    print("Elapsed time: " + str(datetime.timedelta(seconds=elapsed_time)) + "s - size: " + str(item) + "k \r", sep=' ', end='', flush=True, file=sys.stderr)
 
 def start_timer():
     global elapsed_time
@@ -55,12 +57,16 @@ def check_status(status):
 def exit(code):
     stop_timer()
     sys.exit(code)
+    
+def signal_handler(sig, frame):
+    exit(0)
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description="""Fetch data from a Web Server using the environment variable 'CREDENTIALS' or %USERPROFILE%\_netrc
 For Windows OS only""")
-    parser.add_argument("url", action="store", help="URL to fetch")
     parser.add_argument("media", action="store", help="Media type. Typically 'application/json' or 'text/csv'")
+    parser.add_argument("url", action="store", help="URL to fetch")    
     parser.add_argument("-o", "--output", dest="output", action="store", help="Output file path, a verbose mode is also enabled")
     parser.add_argument("-r", "--retry", type=int, dest="retry", action="store", help="Number of retries in case of network error", default=5)
     args = parser.parse_args()
@@ -78,7 +84,6 @@ For Windows OS only""")
         exit(0)
     else:
         start_timer()
-        print(credentials)
         response =requests.get(args.url, headers={'accept': args.media, 'user-agent':'python'}, auth=(credentials[0], credentials[1]), stream=True)
         check_status(response.status_code)
         with open(args.output, 'wb') as f:
