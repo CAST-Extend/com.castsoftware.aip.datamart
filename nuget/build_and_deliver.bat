3ceafd10eee250c53a890435d369f6aac8426224
ero <e.roy@castsoftware.com>
2020-04-07 12:28:33 +0200

    create build procedure

--- nuget/build_and_deliver.bat
+++ nuget/build_and_deliver.bat
@@ -10,18 +10,13 @@ for /f "delims=/" %%a in ('cd') do set WORKSPACE=%%a
 for %%a in (%0) do set CMDDIR=%%~dpa
 set CMDPATH=%0
 
-cd /d %WORKSPACE%
-
-set VERSION=1.1.2
-set ID=com.castsoftware.aip.console.tools
-
-set PATH=%PATH%;c:\Tools\Git\usr\bin;C:\CAST-Caches\Win64
-
 :: Checking arguments
-set BUILDDIR=
-set ACTOOLSDIR=
-set PACKDIR=
+set TOOLS_DIR=
+set SRC_DIR=
+set PACK_DIR=
 set BUILDNO=
+set VERSION=
+set WORKSPACE=
 set NOPUB=false
 
 :LOOP_ARG
@@ -35,19 +30,19 @@ set NOPUB=false
 goto LOOP_ARG
 
 :CHECK_ARGS
-if not defined BUILDDIR (
+if not defined TOOLS_DIR (
 	echo.
-	echo No "builddir" defined !
+	echo No "tools_dir" defined !
 	goto endclean
 )
-if not defined ACTOOLSDIR (
+if not defined SRC_DIR (
 	echo.
-	echo No "ACTOOLSDIR" defined !
+	echo No "src_dir" defined !
 	goto endclean
 )
-if not defined PACKDIR (
+if not defined PACK_DIR (
 	echo.
-	echo No "PACKDIR" defined !
+	echo No "pack_dir" defined !
 	goto endclean
 )
 if not defined BUILDNO (
@@ -55,57 +50,78 @@ if not defined BUILDNO (
 	echo No "buildno" defined !
 	goto endclean
 )
+if not defined VERSION (
+	echo.
+	echo No "version" defined !
+	goto endclean
+)
+if not defined WORKSPACE (
+	echo.
+	echo No "workspace" defined !
+	goto endclean
+)
 
-for %%a in (%BUILDDIR% %ACTOOLSDIR%) do (
+set PATH=%PATH%;c:\Tools\Git\usr\bin;C:\CAST-Caches\Win64
+if not exist %TEMP% mkdir %TEMP%
+set TMPFIC=%TEMP%\build_and_deliver.txt
+set ID=com.castsoftware.aip.datamart
+
+for %%a in (%WORKSPACE% %TOOLS_DIR% %SRC_DIR%) do (
     if not exist %%a (
         echo.
         echo ERROR: Folder %%a does not exist
         goto endclean
     )
 )
+cd /d %WORKSPACE%
 
-
-for %%a in (%PACKDIR%) do (
+for %%a in (%PACK_DIR%) do (
     if exist %%a rmdir /s /q %%a
     mkdir %%a
     if errorlevel 1 goto endclean
 )
 
-pushd %PACKDIR%
-for /f "delims=/" %%a in ('cd') do set PACKDIR=%%a
+echo Creating and zipping component package
+pushd %PACK_DIR%
+for /f "delims=/" %%a in ('cd') do set PACK_DIR=%%a
+robocopy /mir /nfl /ndl /njh /njs /nc /ns %SRC_DIR% . -xd nuget -xd .git -xf .gitattributes
+if errorlevel 8 goto endclean
+if exist *.zip del /q *.zip
+set CMD=7z.exe a -sdel -r -mx5 ..\%ID%.%VERSION%.zip .\
+echo Executing command:
+echo %CMD%
+call %CMD% >%TMPFIC% 2>&1
+if errorlevel 1 (
+    type %TMPFIC%
+    goto endclean
+)
 popd
-
-set ZIPNAME=%ID%.%VERSION%.zip
-pushd %ACTOOLSDIR%\aip-console-tools-cli\target
-7z.exe a -y -r %PACKDIR%/%ZIPNAME% aip-console-tools-cli*.jar
-if errorlevel 1 goto endclean
-popd
-pushd %ACTOOLSDIR%\aip-console-jenkins\target
-7z.exe a -y -r %PACKDIR%/%ZIPNAME% aip-console-jenkins*.hpi
-if errorlevel 1 goto endclean
-popd
-
-xcopy /f /y %ACTOOLSDIR%\nuget\package_files\plugin.nuspec %PACKDIR%
+mv.exe %ID%.%VERSION%.zip %PACK_DIR%
 if errorlevel 1 goto endclean
 
-sed -i 's/_THE_VERSION_/%VERSION%/' %PACKDIR%/plugin.nuspec
+xcopy /f /y %SRC_DIR%\nuget\package_files\plugin.nuspec %PACK_DIR%
 if errorlevel 1 goto endclean
-sed -i 's/_THE_ID_/%ID%/' %PACKDIR%/plugin.nuspec
+
+sed -i 's/_THE_VERSION_/%VERSION%/' %PACK_DIR%/plugin.nuspec
+if errorlevel 1 goto endclean
+sed -i 's/_THE_VERSION_/%VERSION%/' %PACK_DIR%/plugin.nuspec
+if errorlevel 1 goto endclean
+sed -i 's/_THE_ID_/%ID%/' %PACK_DIR%/plugin.nuspec
 if errorlevel 1 goto endclean
  
 :: ========================================================================================
 :: Nuget packaging
 :: ========================================================================================
-set CMD=%BUILDDIR%\nuget_package_basics.bat outdir=%PACKDIR% pkgdir=%PACKDIR% buildno=%BUILDNO% nopub=%NOPUB% is_component=true
+set CMD=%TOOLS_DIR%\nuget_package_basics.bat outdir=%PACK_DIR% pkgdir=%PACK_DIR% buildno=%BUILDNO% nopub=%NOPUB% is_component=true
 echo Executing command:
 echo %CMD%
 call %CMD%
 if errorlevel 1 goto endclean
 
-for /f "tokens=*" %%a in ('dir /b %PACKDIR%\com.castsoftware.*.nupkg') do set PACKPATH=%PACKDIR%\%%a
+for /f "tokens=*" %%a in ('dir /b %PACK_DIR%\com.castsoftware.*.nupkg') do set PACKPATH=%PACK_DIR%\%%a
 if not defined PACKPATH (
 	echo .
-	echo ERROR: No package was created : file not found %PACKDIR%\com.castsoftware.*.nupkg ...
+	echo ERROR: No package was created : file not found %PACK_DIR%\com.castsoftware.*.nupkg ...
 	goto endclean
 )
 if not exist %PACKPATH% (
@@ -126,7 +142,7 @@ if errorlevel 1 (
 :: ========================================================================================
 :: Nuget checking
 :: ========================================================================================
-set CMD=%GROOVYEXE% %BUILDDIR%\nuget_package_verification.groovy --packpath=%PACKPATH%
+set CMD=%GROOVYEXE% %TOOLS_DIR%\nuget_package_verification.groovy --packpath=%PACKPATH%
 echo Executing command:
 echo %CMD%
 call %CMD%
