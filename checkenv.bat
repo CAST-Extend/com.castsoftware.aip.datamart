b6fe9b7c8fc6b3d44df1264107a19549b335768c
ppa <p.pares@castsoftware.com>
2020-05-02 13:29:15 +0200

    Check PSQL,VACUUMDB variables

--- checkenv.bat
+++ checkenv.bat
@@ -16,8 +16,8 @@ SET PATH=%INSTALLATION_FOLDER%\thirdparty;%PATH%
 :CHECK_EXE
 WHERE PYTHON > nul 2> nul || (echo Python is not found & EXIT /b /1)
 WHERE CURL > nul 2> nul || (echo CURL is not found & EXIT /b /1)
-WHERE PSQL > nul 2> nul || (echo PSQL is not found & EXIT /b /1)
-WHERE VACUUMDB > nul 2> nul || (echo VACUUMDB is not found & EXIT /b /1)
+WHERE PSQL > nul 2> nul || (call :EXIST PSQL "%PSQL%") || (echo PSQL is not found & EXIT /b /1)
+WHERE VACUUMDB > nul 2> nul || (call :EXIST VACUUMDB "%VACUUMDB%") || (echo VACUUMDB is not found & EXIT /b /1)
 
 python utilities\check_python_version.py || EXIT /b 1
 
@@ -40,3 +40,9 @@ IF NOT DEFINED LOG_FILE SET LOG_FILE=%INSTALLATION_FOLDER%\log\ETL-%NOW%.log
 SET PSQL_OPTIONS=-d %_DB_NAME% -h %_DB_HOST% -U %_DB_USER% -p %_DB_PORT% --set=ON_ERROR_STOP=true
 SET VACUUM_OPTIONS=-h %_DB_HOST% -U %_DB_USER% -p %_DB_PORT%
 
+goto :EOF
+
+:EXIST
+    IF NOT DEFINED %~1 EXIT /b 1
+    IF NOT EXIST %~2 EXIT /b 1
+    EXIT /b 0
