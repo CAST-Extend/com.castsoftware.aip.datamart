a8b9381812cd1ea967139b3e68873d4e2c2340df
ppa <p.pares@castsoftware.com>
2021-06-18 10:46:19 +0200

    remove the precedence of C:\windows\system32\curl.exe on the third-party directory for curl

--- utilities/curl.py
+++ utilities/curl.py
@@ -23,13 +23,12 @@ def full_executable_path(invoked):
 def main():    
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description="""Run CURL to fetch data from a Web Server using the environment variable 'CREDENTIALS' or %USERPROFILE%\_netrc
 For Windows OS only""")
-    parser.add_argument(full_executable_path("curl.exe"), action="store", help="Curl binary path")
     parser.add_argument("mediatype", action="store", help="Media type. Typically 'application/json' or 'text/csv'")
     parser.add_argument("url", action="store", help="URL to fetch")    
     parser.add_argument("-o", "--output", dest="output", action="store", help="Output file path")
     args = parser.parse_args()
 
-    curl_args = [args.curl, '-c', 'cookies.txt', '-b', 'cookies.txt', '--retry', '5', '--no-buffer', '-f', '-k', '-H', 'Accept: ' + args.mediatype, '-H', 'X-Client: Datamart']
+    curl_args = [full_executable_path("curl.exe"), '-c', 'cookies.txt', '-b', 'cookies.txt', '--retry', '5', '--no-buffer', '-f', '-k', '-H', 'Accept: ' + args.mediatype, '-H', 'X-Client: Datamart']
     credentials=os.getenv("CREDENTIALS")
     apikey=os.getenv("APIKEY")
     if credentials:
@@ -42,9 +41,10 @@ For Windows OS only""")
     if output:
         curl_args += ['-o', output]
     curl_args +=  [args.url]
+    #print(curl_args)
     exit_code = subprocess.run(curl_args).returncode
     if exit_code == 22:
-        print("on error (470): check the credentials", file=sys.stderr)
+        print("on error (470/401): check the credentials", file=sys.stderr)
         print("on error (400): check the REST API version", file=sys.stderr)
         print("on error (404): check the domain name", file=sys.stderr)
     sys.exit(exit_code)
