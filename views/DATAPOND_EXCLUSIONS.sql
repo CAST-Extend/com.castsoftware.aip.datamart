a4e8e5d2f842543371ed97e7b5963f5a0a6bccfe
ppa <p.pares@castsoftware.com>
2021-08-27 10:03:40 +0200

    Add DATAPOND_EXCLUSIONS view

---
+++ views/DATAPOND_EXCLUSIONS.sql
@@ -0,0 +1,16 @@
+DROP VIEW IF EXISTS :schema.datapond_exclusions CASCADE;
+CREATE OR REPLACE VIEW :schema.datapond_exclusions AS 
+SELECT 
+    x.application_name AS appname,
+    'Exclusions' AS exclusionbs,
+    x.metric_id AS exclusions_metric_id,
+    x.rule_name AS exclusions_metric_name,
+    x.user_name AS exclusions_admin_id,
+    x.comment AS exclusions_comments,
+    s.internal_id AS exclusions_added_since_snapshot,
+    count(*) AS count_of_exclusions,
+    x.last_update_date AS last_updated_date
+FROM :schema.usr_exclusions x
+JOIN :schema.DIM_SNAPSHOTS s on s.SNAPSHOT_ID = x.exclusion_snapshot_id
+group by 1,2,3,4,5,6,7,9
+
