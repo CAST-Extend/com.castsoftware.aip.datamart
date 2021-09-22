076ec0c02947ca5b3e1e6624065c8571b42eca24
ppa <p.pares@castsoftware.com>
2021-09-22 16:49:52 +0200

    From a REST API domain, compare number of snapshots with number of snaphots in Datamart

--- utilities/check_new_snapshot.py
+++ utilities/check_new_snapshot.py
@@ -2,7 +2,7 @@ import sys,json,psycopg2,os
 import csv
 import decode
 
-# Read left snapshots from STDIN
+# Read left snapshots from STDIN (REST API)
 left_snapshots = {}
 csv_reader = csv.reader(sys.stdin, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 skip = True
@@ -23,17 +23,11 @@ with open(sys.argv[1]) as csvfile:
     for csv_row in csv_reader:
         application_name = csv_row[0]
         nb_snapshots = csv_row[1]
-        right_snapshots[csv_row[0]] = nb_snapshots
-
-# Compare the number of applications of both sides
-if len(left_snapshots) != len(right_snapshots):
-    sys.exit(1)
+        right_snapshots[csv_row[0]] = int(nb_snapshots)
 
 # Compare the number of snapshots from stdin with the number of snapshots stored in the Datamart tables by application
 for application in left_snapshots:
+    #print("left_snapshots[" + application + "] (" + str(left_snapshots[application]) + ") != right_snapshots[" + application + "] (" + str(right_snapshots[application]) + "))" + " = " + str(left_snapshots[application] != right_snapshots[application]))
     if left_snapshots[application] != right_snapshots[application]:
         sys.exit(1)
 
-for application in right_snapshots:
-    if left_snapshots[application] != right_snapshots[application]:
-        sys.exit(1)
