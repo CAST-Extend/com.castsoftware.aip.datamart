16042c0bd5c89a5ce350b474c582d953c31ab015
ppa <p.pares@castsoftware.com>
2021-09-22 15:30:50 +0200

    Review snapshots sets comparison

--- utilities/check_new_snapshot.py
+++ utilities/check_new_snapshot.py
@@ -3,7 +3,7 @@ import csv
 import decode
 
 # Read left snapshots from STDIN
-snapshots = {}
+left_snapshots = {}
 csv_reader = csv.reader(sys.stdin, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 skip = True
 for csv_row in csv_reader:
@@ -11,23 +11,30 @@ for csv_row in csv_reader:
        skip = False
        continue
     application_name = csv_row[1]
-    if not application_name in snapshots:
-        snapshots[application_name] = 1
+    if not application_name in left_snapshots:
+        left_snapshots[application_name] = 1
     else:
-        snapshots[application_name] += 1
+        left_snapshots[application_name] += 1
 
 # Read right snapshots from CSV File (Datamart table)
-# Compare number of snapshots from stdin with the number of snapshots stored in the Datamart tables by application
-try:
-    csv_reader = csv.reader(sys.argv[1], delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+right_snapshots = {}
+with open(sys.argv[1]) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for csv_row in csv_reader:
-        try:
-            if snapshots[csv_row[0]] != csv_row[1]:
-                sys.exit(1)
-        except:
-            continue
-    sys.exit(0)
-except Exception as e:
-    print("Unable to connect to the datamart database" )
-    print(e)
-    sys.exit(2)
+        application_name = csv_row[0]
+        nb_snapshots = csv_row[1]
+        right_snapshots[csv_row[0]] = nb_snapshots
+        sys.exit(0)
+
+# Compare the number of applications of both sides
+if len(left_snapshots) != len(right_snapshots):
+    sys.exit(1)
+
+# Compare the number of snapshots from stdin with the number of snapshots stored in the Datamart tables by application
+for application in left_snapshots:
+    if left_snapshots[application] != right_snapshots[application]:
+        sys.exit(1)
+
+for application in right_snapshots:
+    if left_snapshots[application] != right_snapshots[application]:
+        sys.exit(1)
