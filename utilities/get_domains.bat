10e156d478b0a106eb4a0d8401db9c730ddf3a10
ppa <p.pares@castsoftware.com>
2021-09-27 18:02:36 +0200

    Return exit code in case of failure

--- utilities/get_domains.bat
+++ utilities/get_domains.bat
@@ -1,8 +1,8 @@
 @echo off
 (echo "" > %2) || goto :FAIL
 (python utilities\curl.py application/json "%1" | python utilities\filter_domains.py > %2)
-EXIT /b 0
+EXIT /b %ERRORLEVEL%
 
 :FAIL
-echo Access is denied to write into %2 file in current directory
+echo Access is denied to write %2 file into current directory
 EXIT /b 1
